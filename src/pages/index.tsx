dfc7185866b6bd3ade9bab0d01ba81b30ad92617
k-arikawa0312 <s1F102402710@iniad.org>
2024-04-23 01:13:34 +0900

    希望

---
+++ src/pages/index.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import styles from './index.module.css';
+
+const directions = [
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [1, -1],
+  [1, 0],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+];
+
+// const flipStones = (board: number[][], turnColor: number, x: number, y: number) => {};
+
+const Home = () => {
+  const [turncolor, setturncolor] = useState(1);
+  const [board, setboard] = useState([
+    //空白0 白1 黒2 row=行 for 調べる
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 1, 2, 0, 0, 0],
+    // [0, 0, 0, 2, 1, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    // [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 2, 1],
+    [0, 0, 0, 0, 0, 1, 2, 1],
+    [0, 0, 0, 0, 2, 1, 2, 1],
+    [0, 0, 0, 1, 2, 1, 2, 1],
+    [0, 0, 2, 1, 2, 1, 2, 1],
+    [0, 2, 1, 2, 1, 2, 1, 2],
+  ]);
+
+  const clickHandler = (x: number, y: number) => {
+    console.log(x, y);
+    let n = 0;
+    let m = 0;
+
+    const newboard = structuredClone(board);
+    while (n < 6) {
+      n = n + 1;
+      if (board[y + n] !== undefined && board[y + n][x] === 2 / turncolor) {
+        if (
+          board[y + 1 + n] !== undefined &&
+          board[y + 1 + n][x] === turncolor &&
+          board[y + 1][x] !== 0
+        ) {
+          newboard[y][x] = turncolor;
+          while (n > m) newboard[y + n][x] = turncolor;
+          setturncolor(2 / turncolor);
+          m = m + 1;
+        }
+      }
+    }
+    setboard(newboard);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          )),
+        )}
+      </div>
+    </div>
+  );
+};
+export default Home;
