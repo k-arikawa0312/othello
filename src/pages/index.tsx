3fe1b8c24df56bcf461f5f8a7b0ed676f4d77269
k-arikawa0312 <s1F102402710@iniad.org>
2024-05-02 01:05:09 +0900

    レベル2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import styles from './index.module.css';
-let canPut = 0; //0でおけない1でおける
 
 const directions = [
   [0, 1],
@@ -12,23 +11,72 @@ const directions = [
   [-1, 0],
   [-1, 1],
 ];
+function canPut(x: number, y: number, board: number[][], turnColor: number) {
+  console.log(x, y);
+  console.log(board);
+
+  if (board[y][x] === 1 || board[y][x] === 2) return;
+  for (const direction of directions) {
+    const currentPos = [0, 0];
+
+    const [dy, dx] = direction;
+    // console.log(dy, dx);
+    // console.log(direction);
+    currentPos[1] = x + dx;
+    currentPos[0] = y + dy;
+    let findOpponent: boolean = false;
+
+    let count = 0;
+    // currentPos[0] >= 0 && currentPos[0] < 8 && currentPos[1] >= 0 && currentPos[1] < 8
+    while (count < 8 && board[currentPos[0]] !== undefined) {
+      count++;
+      // console.log(currentPos);
+      console.log(currentPos);
+      const stone = board[currentPos[0]][currentPos[1]];
+      console.log(stone);
+      // console.log([currentPos[1]][currentPos[0]]);
+      if (stone === 2 / turnColor) {
+        console.log(10);
+        findOpponent = true;
+        currentPos[1] += dx;
+        currentPos[0] += dy;
+        continue;
+      }
+      if (stone === 0 || stone === 3) {
+        console.log('no');
+        break;
+      }
+      if (stone === turnColor) {
+        if (findOpponent) {
+          console.log('passed');
+          return true;
+        }
+      }
+      break;
+    }
+
+    // currentPos[0] += dx;
+    // currentPos[1] += dy;
+  }
+}
 
 const Home = () => {
   const [turncolor, setturncolor] = useState(1);
   const [board, setboard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
   const clickHandler = (x: number, y: number) => {
-    if (board[y][x] !== 0) return;
+    if (board[y][x] === 1 || board[y][x] === 2) return;
     const newboard = structuredClone(board);
+
     for (const direction of directions) {
       const currentPos = [0, 0];
       const [dx, dy] = direction;
@@ -36,16 +84,13 @@ const Home = () => {
       currentPos[0] = x + dx;
       currentPos[1] = y + dy;
       let findOpponent: boolean = false;
-      canPut = 0;
+
       while (currentPos[0] >= 0 && currentPos[0] < 8 && currentPos[1] >= 0 && currentPos[1] < 8) {
         const stone = board[currentPos[1]][currentPos[0]];
 
-        if (stone === 0) break;
+        if (stone === 0 || stone === 3) break;
         if (stone === turncolor) {
           if (findOpponent) {
-            newboard[y][x] = turncolor;
-            newboard[y + dy][x + dx] = turncolor;
-            canPut = 1;
             for (let n = 0; n < 8; n++) {
               if (
                 x + dx * n < 0 ||
@@ -57,7 +102,7 @@ const Home = () => {
                 break;
               newboard[y + dy * n][x + dx * n] = turncolor;
             }
-            setboard(newboard);
+
             setturncolor(2 / turncolor);
           }
           break;
@@ -68,8 +113,24 @@ const Home = () => {
         currentPos[1] += dy;
       }
     }
-  };
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (newboard[i][j] === 3) {
+          newboard[i][j] = 0;
+        }
+      }
+    }
+    for (let ty = 0; ty < 8; ty++) {
+      for (let tx = 0; tx < 8; tx++) {
+        if (canPut(tx, ty, newboard, 3 - turncolor)) {
+          newboard[ty][tx] = 3;
+        }
+      }
+    }
+    // canPut(7, 4, newboard, 1);
 
+    setboard(newboard);
+  };
   const blackCount = board.flat().filter((cell) => cell === 1).length;
   const whiteCount = board.flat().filter((cell) => cell === 2).length;
   return (
@@ -89,13 +150,13 @@ const Home = () => {
         {board.map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              {color !== 0 && (
+              {color === 3 && <div className={styles.area} />}
+              {color !== 0 && color !== 3 && (
                 <div
                   className={styles.stone}
                   style={{ background: color === 1 ? '#000' : '#fff' }}
                 />
               )}
-              <div />
             </div>
           )),
         )}
